82cda73cb1a2829c93b15f992169a3d1ae29aa12
robertdpowell <robertdpowell@gmail.com>
2021-07-24 23:44:48 +0100

    initial commit

---
+++ Gemfile
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+source "https://rubygems.org"
+
+git_source(:github) {|repo_name| "https://github.com/#{repo_name}" }
+
+# gem "rails"
+
+gem 'jekyll', '3.9.0'
+gem "github-pages", "~> 208"
+
+group :jekyll_plugins do
+gem 'jekyll-paginate'
+end
+gem "webrick", "~> 1.7"
---
+++ _site/script.js
@@ -0,0 +1,74 @@
+const form = document.getElementById("form");
+const input = document.getElementById("input");
+const todosUL = document.getElementById("todos");
+const todos = JSON.parse(localStorage.getItem("todos"));
+const clear = document.getElementById("clear");
+
+//if there are todos in local storage, get them and for each one, perform 'addTodo'
+if (todos) {
+  todos.forEach((todo) => {
+    addTodo(todo);
+  });
+}
+
+//once all todos are loaded, listen for new ones
+form.addEventListener("submit", (e) => {
+  //what is e?
+  e.preventDefault(); //what is this?
+  addTodo();
+});
+
+clear.addEventListener("click", () => {
+  localStorage.clear();
+  location.reload();
+});
+
+function addTodo(todo) {
+  let todoText = input.value;
+
+  //if we are performing function with an input of a todo, we use the todo text already present in ls
+  //as there is no input to capture
+  if (todo) {
+    todoText = todo.text;
+  }
+
+  //all scenarios go through this route.
+  if (todoText) {
+    const todoEl = document.createElement("li");
+    if (todo && todo.completed) {
+      todoEl.classList.add("completed");
+    }
+
+    todoEl.innerText = todoText;
+
+    todoEl.addEventListener("click", () => {
+      todoEl.classList.toggle("completed");
+      updateLS();
+    });
+
+    todoEl.addEventListener("contextmenu", (e) => {
+      e.preventDefault();
+      todoEl.remove();
+      updateLS();
+    });
+
+    todosUL.appendChild(todoEl); //add todos to end of list
+    input.value = ""; //reset input box to blank
+    updateLS();
+  }
+}
+
+function updateLS() {
+  const todosEl = document.querySelectorAll("li");
+
+  const todos = [];
+
+  todosEl.forEach((todoEl) => {
+    todos.push({
+      text: todoEl.innerText,
+      completed: todoEl.classList.contains("completed"),
+    });
+  });
+
+  localStorage.setItem("todos", JSON.stringify(todos));
+}
